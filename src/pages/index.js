81f6e7c1dc3a3c5b31592248ab10db997be46f85
Charles-Axel Dein <ca@d3in.org>
2020-04-02 22:55:43 +0200

    Ajoute date dans texte

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 import ContributeMessage from "../components/ContributeMessage"
 
 const IndexPage = ({ data }) => {
-  const { markdownRemark } = data
+  const { markdownRemark, currentBuildDate } = data
   const { html, tableOfContents } = markdownRemark
 
   return (
@@ -25,7 +25,8 @@ const IndexPage = ({ data }) => {
       <p>
         Cette liste est nécessairement limitée, nous avons choisi de ne pas être
         exhaustif mais de vous proposer uniquement des contenus de grande
-        qualité.
+        qualité. Elle est mise à jour en permanence, sa dernière mise à jour a
+        eu lieu le {currentBuildDate.currentDate}.
       </p>
 
       <ContributeMessage />
@@ -48,6 +49,9 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query {
+    currentBuildDate {
+      currentDate
+    }
     markdownRemark(frontmatter: { path: { eq: "/" } }) {
       html
       tableOfContents(maxDepth: 2, absolute: false)
